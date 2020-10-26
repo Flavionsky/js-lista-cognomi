666f5827aca3cd1a42e318ed0206ab9c27e667fd
Flavio Muscente <flaviomuscente@libero.it>
2020-10-26 10:59:54 +0100

    add first letter uppercase

--- js/script.js
+++ js/script.js
@@ -16,13 +16,13 @@ var messageAddEl = document.getElementById("messageAdd");
 var surnameListEl = document.getElementById("surnameList");
 var listTitleEl = document.getElementById("listTile");
 var messagePositionEl = document.getElementById("messagePosition");
+
 // aggiungo il click del bottone ricerca
 
 searchbtnEl.addEventListener("click", function () {
 
-  // prendo il cognome inserito dall'utente
+  //prendo input cognome inserito dall'utente
   var surnameEl = document.getElementById("surnameEntered").value;
-
   //confronto il cognome con quelli nell'archivio
   for (var i = 0; i < surnameContainer.length; i++) {
     if (surnameEl == surnameContainer[i]) {
@@ -42,30 +42,40 @@ searchbtnEl.addEventListener("click", function () {
 // definisco cosa fa il bottone inserisci
 addbtnEl.addEventListener("click", function () {
 
-  // prendiamo il cognome inserito dall'utente
-
+  //prendo input cognome inserito dall'utente
   var surnameEl = document.getElementById("surnameEntered").value;
-
-  // e lo mettiamo dentro l'Array
+  //inserisce il cognome all'interno dell'array
   surnameContainer.push(surnameEl);
 
   //output del messaggio
   messageAddEl.innerHTML = "Cognome inserito correttamente!";
   messageAddEl.style.backgroundColor ="green";
   listTitleEl.style.display = "block";
-  //ordino la lista dei cognomi
-  surnameContainer.sort();
-  //definisco posizione utente iniziale
-  var positionSurname = 0;
-  //metto in output la lista aggiornata
+
+  // trasformo tutti gli elementi dell array in stringhe lower cased
+  var surnamesLowerCase = [];
   for (var i = 0; i < surnameContainer.length; i++){
-    surnameListEl.innerHTML += "<li>" + surnameContainer[i] + "</li>";
-    // calcolo posizione del cognome inserito dall'utente
-    if (surnameContainer[i] == surnameEl){
-      positionSurname = i + 1;
-    }
+    surnamesLowerCase.push(surnameContainer[i].toLowerCase());
   }
-  //visualizzo il messaggio e la relativa posizione
-  messagePosition.style.display = "block";
-  messagePositionEl.innerHTML += positionSurname;
+  //ordino la lista dei cognomi
+  surnamesLowerCase.sort();
+
+  // Stampo l'output dei dati che ho elaborato
+  var i = 0;
+  while( i < surnamesLowerCase.length ){
+
+    // stampa l'elemento <li> nell'HTML
+    var firstLetter = surnamesLowerCase[i].charAt(0);
+    firstLetter = firstLetter.toUpperCase();
+    var surnameWithUpperCapital = firstLetter + surnamesLowerCase[i].slice(1);
+    document.getElementById('surnameList').innerHTML += '<li>'+surnameWithUpperCapital+'</li>';
+
+    // memorizza l'indice della stringa inserita dall'utente nell'array ordinato
+    if( surnamesLowerCase[i] == surnameEl.toLowerCase() ){
+      document.getElementById('messagePosition').innerHTML += i + 1;
+      messagePosition.style.display = "block";
+    }
+
+    i++;
+  }
 });
